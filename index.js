8a9abb5ae120a03830aae1be0a82cf177b8ebdba
Tao Chen <taochensyd@gmail.com>
2023-11-17 16:16:20 +1100

    add sample json output

--- index.js
+++ index.js
@@ -54,6 +54,96 @@ fetchPrinterDetails();
 setInterval(fetchPrinterDetails, 600000); // Fetch printer details every 10 minutes
 
 app.get('/printer/tonerlevel', (req, res) => {
+
+    /*
+        [
+    {
+        "name": "HP-M880 Export",
+        "ip": "192.168.0.25",
+        "tonerLevelBlack": "2%",
+        "tonerLevelCyan": "20%",
+        "tonerLevelMagenta": "2%",
+        "tonerLevelYellow": "100%",
+        "drumLevelBlack": "3%",
+        "drumLevelCyan": "",
+        "drumLevelMagenta": "2%",
+        "drumLevelYellow": "50%",
+        "fuserKitLevel": "10%",
+        "documentFeederKitLevel": "50%"
+    },
+    {
+        "name": "HP-M880 QA",
+        "ip": "192.168.0.26",
+        "tonerLevelBlack": "20%",
+        "tonerLevelCyan": "50%",
+        "tonerLevelMagenta": "2%",
+        "tonerLevelYellow": "4%",
+        "drumLevelBlack": "80%",
+        "drumLevelCyan": "2%",
+        "drumLevelMagenta": "2%",
+        "drumLevelYellow": "70%",
+        "fuserKitLevel": "10%",
+        "documentFeederKitLevel": "20%"
+    },
+    {
+        "name": "HP-M880 OP",
+        "ip": "192.168.0.27",
+        "tonerLevelBlack": "50%",
+        "tonerLevelCyan": "40%",
+        "tonerLevelMagenta": "2%",
+        "tonerLevelYellow": "20%",
+        "drumLevelBlack": "2%",
+        "drumLevelCyan": "70%",
+        "drumLevelMagenta": "6%",
+        "drumLevelYellow": "80%",
+        "fuserKitLevel": "2%",
+        "documentFeederKitLevel": "90%"
+    },
+    {
+        "name": "HP-M880 Local",
+        "ip": "192.168.0.28",
+        "tonerLevelBlack": "40%",
+        "tonerLevelCyan": "40%",
+        "tonerLevelMagenta": "10%",
+        "tonerLevelYellow": "80%",
+        "drumLevelBlack": "60%",
+        "drumLevelCyan": "70%",
+        "drumLevelMagenta": "10%",
+        "drumLevelYellow": "70%",
+        "fuserKitLevel": "2%",
+        "documentFeederKitLevel": "100%"
+    },
+    {
+        "name": "HP-M880 Warehouse",
+        "ip": "192.168.0.29",
+        "tonerLevelBlack": "50%",
+        "tonerLevelCyan": "30%",
+        "tonerLevelMagenta": "80%",
+        "tonerLevelYellow": "90%",
+        "drumLevelBlack": "",
+        "drumLevelCyan": "60%",
+        "drumLevelMagenta": "60%",
+        "drumLevelYellow": "70%",
+        "fuserKitLevel": "30%",
+        "documentFeederKitLevel": "100%"
+    },
+    {
+        "name": "HP-M880 L2 North",
+        "ip": "192.168.0.30",
+        "tonerLevelBlack": "20%",
+        "tonerLevelCyan": "60%",
+        "tonerLevelMagenta": "80%",
+        "tonerLevelYellow": "100%",
+        "drumLevelBlack": "30%",
+        "drumLevelCyan": "60%",
+        "drumLevelMagenta": "100%",
+        "drumLevelYellow": "60%",
+        "fuserKitLevel": "30%",
+        "documentFeederKitLevel": "50%"
+    }
+]
+    */
+
     res.json(output);
 });
 
